c9ee1e43f20557b3ade7a41792e0f581e664ff17
Alexis Ambray <alexisambray@gmail.com>
2021-03-26 19:23:54 +0800

    Created skeleton program

---
+++ temperature.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float convertFtoC(float fahrenheit) {
+  /* write your code here */ }
+
+  /*write the return type here */ convertCtoK(/*write the parameters here*/) {
+    /* write your code here */ }
+  int main() {
+    float fahrenheit, celsius, kelvin;
+
+    printf("Fahrenheit ");
+    scanf("%f", &fahrenheit);
+
+    celsius = convertFtoC(fahrenheit);
+    kelvin = /* call the function convertCtoK here */
+        printf("\nCelsius: %.2f\n", celsius);
+    printf("Kelvin: %.2f\n", kelvin);
+
+    return 0;
+  }
