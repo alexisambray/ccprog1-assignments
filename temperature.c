01659286edec4da742f78d2e7e922a269aaec5da
Alexis Ambray <alexisambray@gmail.com>
2021-03-26 19:58:20 +0800

    Refactorings

--- temperature.c
+++ temperature.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-float convertFtoC(float fahrenheit) {
-  return 5.0 / 9.0 * (fahrenheit - 32);
-}
-
-float convertCtoK(celsius) {
-  return celsius + 273;
-}
+float convertFahrToCelsius(const float fahrenheit);
+float convertCelsiusToKelvin(const float celsius);
 
 int main() {
   float fahrenheit, celsius, kelvin;
@@ -14,10 +9,18 @@ int main() {
   printf("Fahrenheit: ");
   scanf("%f", &fahrenheit);
 
-  celsius = convertFtoC(fahrenheit);
-  kelvin = convertCtoK(celsius);
+  celsius = convertFahrToCelsius(fahrenheit);
+  kelvin = convertCelsiusToKelvin(celsius);
   printf("\nCelsius: %.2f\n", celsius);
   printf("Kelvin: %.2f\n", kelvin);
 
   return 0;
+}
+
+float convertFahrToCelsius(const float fahrenheit) {
+  return 5.0 / 9.0 * (fahrenheit - 32);
+}
+
+float convertCelsiusToKelvin(const float celsius) {
+  return celsius + 273;
 }
